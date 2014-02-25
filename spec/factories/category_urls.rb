e87e60b0aaf6fa8be12217ca0836147867455bc9
Joan Roig <roigarderiu@gmail.com>
2014-02-24 22:01:47 -0800

    Removed unused files

--- spec/factories/category_urls.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :category_url do
-    source
-    last_parsed "2014-02-05 08:46:18"
-    url "http://www.gsmarena.com/nokia-phones-1.php"
-  end
-end
