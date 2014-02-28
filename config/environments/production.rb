b463de11c3a5e3947f38ac60fc0b929fca153e20
Joan Roig <roigarderiu@gmail.com>
2014-02-27 20:44:35 -0800

    Added digest for assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ PhoneFindr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -20,7 +20,7 @@ PhoneFindr::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -30,7 +30,7 @@ PhoneFindr::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
